c55a1eb6e7cb113982d54b5e41321ef278c48342
Elias Vakkuri <elias.vakkuri@brainflight.fi>
2023-02-23 13:09:44 +0200

    feat: Code to reproduce

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,24 @@
+import type { GetServerSideProps } from "next"
+
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+export default function Home(props: any) {
+  return (
+    <>
+      <p>{props.ip}</p>
+      <p>{process.env.NEXT_PUBLIC_API_URL}</p>
+      {/* <p>{process.env.API_URL || "(Not found)"}</p> */}
+    </>
+  )
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  console.log(process.env.API_URL)
+  const resp = await fetch(process.env.API_URL, {headers: {
+    "Accept": "application/json"
+  }})
+  return {
+    props: {
+      ip: (await resp.json()).ip
+    }
+  }
+} 
